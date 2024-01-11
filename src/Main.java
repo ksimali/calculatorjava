30f341a4d06d1f40ff6ec3dba86a36e6b98da7f0
Cassim <cassim.ali@outlook.fr>
2024-01-10 22:00:10 -0500

    Saisi des données par l'utilisateur: - Creation du Scanner -Declaration des variables -Saisi du 1er nombre -Saisi du 2nd nombre
    -

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //Création du scanner pour lire depuis le clavier
+        Scanner scanner = new Scanner(System.in);
+        // Declaration des variables
+        int nombre1, nombre2, addition, multiplication, soustraction, division;
+        String operation;
+
+        //Saisi du 1er nombre
+        System.out.println("Saisir un premier nombre: ");
+        nombre1 = scanner.nextInt();
+
+        //Saisi du 2nd nombre
+        System.out.println("Saisir un second nombre: ");
+        nombre2 = scanner.nextInt();
+
 
     }
 }
